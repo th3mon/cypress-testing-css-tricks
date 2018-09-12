affb306e363334126c4aab2b5d984421a7c7c14d
th3mon <p.szelenberger@gmail.com>
2018-09-12 21:42:54 +0200

    Test site search
    [DRY] Move site address to Cypres config file

--- cypress/integration/sample-spec.js
+++ cypress/integration/sample-spec.js
@@ -1,6 +1,6 @@
 describe('CSS-Tricks home page', () => {
   beforeEach(() => {
-    cy.visit('https://css-tricks.com/');
+    cy.visit('/');
   });
 
   it('contains "CSS-Tricks" in the title', () => {
@@ -28,6 +28,26 @@ describe('CSS-Tricks home page', () => {
 
     it('has visible mobile menu toggle', () => {
       cy.get('#mobile-menu-toggle').should('be.visible');
+    });
+  });
+
+  describe('site search', () => {
+    beforeEach(() => {
+      cy.get('.search-field').type('flexbox{enter}');
+    });
+
+    it('requests the results', () => {
+      cy.url().should('include', '?s=flexbox');
+    });
+
+    describe('Search results page', () => {
+      beforeEach(() => {
+        cy.visit('/?s=flexbox');
+      });
+
+      it('displays search results', () => {
+        cy.get('.search-grid-list li').should('exist');
+      });
     });
   });
 });
